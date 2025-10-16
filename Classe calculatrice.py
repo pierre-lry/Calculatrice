eec29b2caf783010cd3d77737833c49d12482b66
p.l <pierre.lry31@gmail.com>
2025-10-16 11:29:33 +0200

    Class calculatrice

--- Classe calculatrice.py
+++ Classe calculatrice.py
@@ -2,8 +2,8 @@ import math
 class calculatrice:
     def __init__(self):
         self.resultat = 0
-    def addition(self,n1):
-        self.resultat += n1
+    def addition(self,n2):
+        self.resultat += n2
     def multiplication(self,n1):
         self.resultat *= n1
     def soustraction(self,n1):
